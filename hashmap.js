9a9d3bb191205df5fd3c981cef42a8b06b0d9fd2
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-02-13 19:39:23 +0100

    Add class Hashmap and function hash(key)
    -The function hash(key) takes a key and produces a hash code with it.
    -By applying a modulo % operator on each iteration we prevent the output
    from becoming inaccurate and larger than the bucket's length.

---
+++ hashmap.js
@@ -0,0 +1,20 @@
+// if (index < 0 || index >= buckets.length) {
+//     throw new Error("Trying to access index out of bound");
+//   }
+
+class Hashmap {
+  constructor() {}
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % 16;
+    }
+    return hashCode;
+  }
+}
+
+let test = new Hashmap();
+console.log(test.hash('Daniel'));
+console.log(test.hash('Lauren'));
+console.log(test.hash('Earnest'));
