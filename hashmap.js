1e643e76f9a8caf015dbf0912858a127a9d71b92
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-02-14 17:48:59 +0100

    Add hashmap class

--- hashmap.js
+++ hashmap.js
@@ -1,19 +1,35 @@
-// if (index < 0 || index >= buckets.length) {
-//     throw new Error("Trying to access index out of bound");
-//   }
-
+import LinkedList from './linked-list.js';
 class Hashmap {
-  constructor() {}
+  constructor() {
+    this.capacity = 16;
+    this.loadFactor = 0.75;
+    this.buckets = new Array(this.capacity);
+  }
   hash(key) {
     let hashCode = 0;
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % 16;
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
     return hashCode;
   }
-  set(key, value) {}
-  get(key) {}
+  set(key, value) {
+    let index = this.hash(key);
+    if (this.buckets[index]) {
+      this.buckets[index].append({ [key]: value });
+    } else {
+      this.buckets[index] = new LinkedList();
+      this.buckets[index].append({ [key]: value });
+    }
+  }
+  get(key) {
+    let index = this.hash(key);
+    if (!this.buckets[index]) {
+      return null;
+    } else {
+      return this.buckets[index];
+    }
+  }
   has(key) {}
   remove(key) {}
   length() {}
@@ -22,7 +38,14 @@ class Hashmap {
   entries() {}
 }
 
-let test = new Hashmap();
-console.log(test.hash('Daniel'));
-console.log(test.hash('Lauren'));
-console.log(test.hash('Earnest'));
+let map = new Hashmap();
+map.set('Carlos', 'Blue eyes');
+map.set('Diana', 'blonde hair');
+map.set('Sara', 'math teacher');
+map.set('Daniel', '44');
+// console.dir(map.buckets, { depth: null });
+console.dir(map.get('Daniel'), { depth: null });
+
+// if (index < 0 || index >= buckets.length) {
+//   throw new Error('Trying to access index out of bound');
+// }
