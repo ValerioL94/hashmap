3564376a682bc8890d246944df4c8ed4a6cd13dc
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-02-14 17:49:15 +0100

    Add linked-list.js to import relative functions

---
+++ linked-list.js
@@ -0,0 +1,150 @@
+export default class LinkedList {
+  constructor() {}
+  append(value) {
+    if (!this.headNode) this.headNode = new Node(value);
+    else {
+      let current = this.headNode;
+      while (current.nextNode) {
+        current = current.nextNode;
+      }
+      current.nextNode = new Node(value);
+    }
+  }
+  prepend(value) {
+    let node = new Node(value);
+    node.nextNode = null;
+    this.headNode = node;
+  }
+  size() {
+    if (!this.headNode) return 0;
+    let current = this.headNode;
+    let n = 1;
+    while (current.nextNode) {
+      current = current.nextNode;
+      n++;
+    }
+    return n;
+  }
+  head() {
+    if (!this.headNode) return null;
+    return this.headNode;
+  }
+  tail() {
+    if (!this.headNode) return null;
+    let current = this.headNode;
+    let n = 1;
+    while (current.nextNode) {
+      current = current.nextNode;
+      n++;
+    }
+    return current;
+  }
+  at(index) {
+    if (index < 0 || index > this.size()) return null;
+    if (index === 0) return this.headNode;
+    else {
+      let current = this.headNode;
+      let n = 0;
+      while (n < index) {
+        n++;
+        current = current.nextNode;
+      }
+      return current;
+    }
+  }
+  pop() {
+    if (!this.headNode) return null;
+    let previous;
+    let current = this.headNode;
+    while (current.nextNode) {
+      previous = current;
+      current = current.nextNode;
+    }
+    if (current === this.headNode) this.headNode = null;
+    else previous.nextNode = null;
+  }
+  contains(value) {
+    if (!this.headNode) return null;
+    else if (this.headNode.value === value) return true;
+    else {
+      let current = this.headNode;
+      while (current.nextNode) {
+        current = current.nextNode;
+        if (current.value === value) return true;
+      }
+      return false;
+    }
+  }
+  find(value) {
+    if (!this.headNode) return null;
+    else if (this.headNode.value === value) return 0;
+    else {
+      let current = this.headNode;
+      let n = 0;
+      while (current.nextNode) {
+        n++;
+        current = current.nextNode;
+        if (current.value === value) return n;
+      }
+      return null;
+    }
+  }
+  toString() {
+    if (!this.headNode) return null;
+    else {
+      let current = this.headNode;
+      let print = `(${current.value}) -> `;
+      while (current.nextNode) {
+        current = current.nextNode;
+        print += `(${current.value}) -> `;
+      }
+      return (print += 'null');
+    }
+  }
+  insertAt(value, index) {
+    if (index < 0 || index > this.size()) return null;
+    else {
+      let node = new Node(value);
+      if (index === 0) {
+        node.nextNode = this.headNode;
+        this.headNode = node;
+      } else {
+        let previous;
+        let current = this.headNode;
+        let n = 0;
+        while (n < index) {
+          previous = current;
+          current = current.nextNode;
+          n++;
+        }
+        node.nextNode = current;
+        previous.nextNode = node;
+      }
+    }
+  }
+  removeAt(index) {
+    if (index < 0 || index >= this.size()) return null;
+    else {
+      if (index === 0) {
+        this.headNode = this.headNode.nextNode;
+      } else {
+        let previous;
+        let current = this.headNode;
+        let n = 0;
+        while (n < index) {
+          previous = current;
+          current = current.nextNode;
+          n++;
+        }
+        previous.nextNode = current.nextNode;
+      }
+    }
+  }
+}
+
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
