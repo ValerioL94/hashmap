565d3812dd36d2fae47bfd6d7d1538dc5600f3a2
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-02-15 11:49:48 +0100

    Add function entries()
    -This function loops through each element of the hashmap and then,
    if the element is not undefined, it calls the function getEntries()
    on it and returns an array with key:value pairs.

--- hashmap.js
+++ hashmap.js
@@ -93,8 +93,11 @@ class Hashmap {
   }
   entries() {
     let list = [];
+    let print = [];
     this.buckets.forEach((el) => {
-      if (el !== undefined) list.push(el);
+      if (el !== undefined) {
+        list = list.concat(el.getEntries());
+      }
     });
     return list;
   }
@@ -120,4 +123,4 @@ console.log(map.length());
 // console.log(map.buckets);
 console.log(map.keys());
 console.log(map.values());
-// console.log(map.entries());
+console.log(map.entries());
